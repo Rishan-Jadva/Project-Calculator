6a17c81b38268c49f2295f3fc938342daeb84c40
Rishan-Jadva <67826633+Rishan-Jadva@users.noreply.github.com>
2024-08-19 22:54:36 +1000

    HTML essentially completed

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const multiply = (a,b) => {
 const divide = (a,b) => {
     return a / b;
 }
+let firstNumber;
+let operator;
+let secondNumber;
+const operate = (operator, a, b) => {
+    switch (operator){
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+}
